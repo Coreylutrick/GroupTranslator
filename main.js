d8e6edc635b891260e46598e2340a2b56b856c70
Corey Lutrick <coreylutrick@gmail.com>
2018-03-10 13:20:07 -0800

    added in language data

---
+++ main.js
@@ -0,0 +1,41 @@
+var language = 
+[
+    {
+        "merry":"merrui",
+        "christmas":"christmas",
+        "and":"a",
+        "a":"a",
+        "happy":"happui",
+        "new":"siniath",
+        "year":"ín"
+    } 
+    {
+        "merry";"merry",
+        "christmas";"christmas",
+        "and":"'ej",
+        "a":"a",
+        "happy":"happy",
+        "new":"new",
+        "year":"dis"
+    } 
+    {
+        "merry":"merry",
+        "christmas":"christmas",
+        "and":"ma",
+        "a":"a",
+        "happy":"happy",
+        "new":"sash",
+        "year":"firesof"
+    }
+]
+
+
+
+function check()
+
+
+
+
+
+
+
